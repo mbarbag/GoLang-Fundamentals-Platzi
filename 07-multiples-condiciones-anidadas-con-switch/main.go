c9550440524f9eada73bceb6ae97cf620c2a43bf
Manuela Barba <mbarbag@unal.edu.co>
2021-10-05 16:19:34 -0500

    multiples condiciones anidades con switch

---
+++ 07-multiples-condiciones-anidadas-con-switch/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Cuando se va a comparar una sola vble
+	modulo := 5 % 2
+	switch modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
+
+	//Tambien se puede escribir así
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
+
+	//Sin condicion
+	//Cuando se quiere anidar multiples condiciones
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
+	}
+
+}
