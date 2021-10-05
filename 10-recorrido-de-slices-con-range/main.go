f7665b532eb7d9a387cb05d23eaac6bfb3b43731
Manuela Barba <mbarbag@unal.edu.co>
2021-10-05 16:22:41 -0500

    recorrido de slices con range

---
+++ 10-recorrido-de-slices-con-range/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func isPalindromo(text string) {
+	var textReverse string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
+
+func main() {
+	slice := []string{"hola", "que", "hace"}
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
+
+	// Si quiero omitir la i
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
+
+	isPalindromo("cama")
+}
